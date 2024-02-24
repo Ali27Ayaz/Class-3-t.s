f565e1eb87c6a645e2cae3c027a59482d5a0b044
Ali Ayaz <160322443+Ali27Ayaz@users.noreply.github.com>
2024-02-24 14:15:39 +0500

    Add files via upload

---
+++ Class3.js
@@ -0,0 +1,10 @@
+// Asslam o Alaikum
+console.log(" Here are Few Famous Quotes Must read");
+var Quote1 = "Seek knowledge from cradle to the grave ";
+var author = "Our Beloved Prophet ";
+var Quote2 = "The greatest glory in living lies not in never falling, but in rising every time we fall";
+var author2 = "Nelson Mandela";
+console.log();
+console.log("".concat(author, " Once Said, ").concat(Quote1));
+console.log();
+console.log("".concat(author2, " once said, ").concat(Quote2));
---
+++ class3.ts
@@ -0,0 +1,13 @@
+// Asslam o Alaikum
+
+console.log(" Here are Few Famous Quotes Must read")
+
+let FQuote1= "Seek knowledge from cradle to the grave "
+let Author= "Our Beloved Prophet "
+let FQuote2 = "The greatest glory in living lies not in never falling, but in rising every time we fall"
+let Author2= "Nelson Mandela"
+console.log(   )
+console.log(`${Author} Once Said, ${FQuote1}`)
+console.log(   )
+console.log(`${Author2} once said, ${FQuote2}`)
+
